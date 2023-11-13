f0bbc5b393fda81c2ff8838bc39623284a21760e
naeem <naeemdeshmukh96@gmail.com>
2023-11-13 15:25:43 +0530

    Add

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,5 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+app.MapGet("/observations/{zip}", (string zip) =>
+{
+    return Results.Ok(zip);
+});
 
 app.Run();
